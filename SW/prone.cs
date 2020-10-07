6c2450fd9626591ee0b664d7d36e3ae06e4972d6
sangwoo-ryu <59352715+sangwoo-ryu@users.noreply.github.com>
2020-10-07 22:40:37 +0900

    엎드리기
    z키로 구현함 playermove랑 병합해야함

---
+++ SW/prone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prone : MonoBehaviour
+{
+    public Sprite[] stand;
+    public Sprite[] pronesprite;
+    private BoxCollider2D boxCollider;
+    private SpriteRenderer renderer;
+
+
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        renderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Z))
+        {
+            boxCollider.size = new Vector2(0.836f, 0.3f);
+            boxCollider.offset = new Vector2(0f, 0f);
+            renderer.sprite = pronesprite[0];
+        }
+
+        else
+        {
+            boxCollider.size = new Vector2(0.3f, 0.836f);
+            boxCollider.offset = new Vector2(0.05f, 0.02f);
+            renderer.sprite = stand[0];
+        }
+    }
+}
