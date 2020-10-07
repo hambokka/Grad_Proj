8bcd8888e0fd4c57b59e9b1fe06f117d4fe3fdcf
PrivateCain <con2220@naver.com>
2020-10-07 23:27:45 +0900

    Update Movement.cs

--- TW/Movement.cs
+++ TW/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float initialSpeed = 1;
+    public float moveSpeed = 1;
     public float maxSpeed= 2;
     public float JumpForce = 1;
     Rigidbody2D rigid;
@@ -51,16 +51,21 @@ public class Movement : MonoBehaviour
         if (rigid.velocity.x < -1 * maxSpeed)
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
 
+        //movement
+        if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
     }
         
 
     public void FixedUpdate()
     {
         //horizontal movement
-        float h = Input.GetAxisRaw("Horizontal");
+        //float h = Input.GetAxisRaw("Horizontal");
 
         //transform.position += new Vector3(h, 0, 0) * Time.deltaTime * initialSpeed;
-        rigid.AddForce(Vector2.right * h * Time.deltaTime, ForceMode2D.Impulse);
+        //rigid.AddForce(Vector2.right * h * Time.deltaTime, ForceMode2D.Impulse);
 
     }
 }
