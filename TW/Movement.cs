880011304cd991bbe58ad7a324f12fc8eda82e35
PrivateCain <con2220@naver.com>
2020-10-07 23:03:52 +0900

    Update Movement.cs

--- TW/Movement.cs
+++ TW/Movement.cs
@@ -5,30 +5,62 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float initialSpeed = 1f;
-    public float maxSpeed= 2f;
-    public float JumpForce = 2;
-    Rigidbody2D rigid; 
+    public float initialSpeed = 1;
+    public float maxSpeed= 2;
+    public float JumpForce = 1;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
 
-    void Awake()
+    public void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        float h = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(initialSpeed, 0, 0) * Time.deltaTime * initialSpeed;
 
-        if(Input.GetButtonDown("Jump")&& Mathf.Abs(rigid.velocity.y)< 0.001f)
+    
+     public void Update()
+     {
+        //stop speed
+        if (Input.GetButtonUp("Horizontal"))
         {
-            rigid.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        //directional animation
+        if (Input.GetButtonDown("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        //walking animation transition
+        if (Mathf.Abs(rigid.velocity.x) <= 0.05)
+            animator.SetBool("isWalking", false);
+        else
+            animator.SetBool("isWalking", true);
+
+        //jump
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigid.velocity.y) < 0.01f)
+        {
+            rigid.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
         }
 
         if (rigid.velocity.x > maxSpeed)
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        if (rigid.velocity.x < -1*maxSpeed)
+        if (rigid.velocity.x < -1 * maxSpeed)
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+
+    }
+        
+
+    public void FixedUpdate()
+    {
+        //horizontal movement
+        float h = Input.GetAxisRaw("Horizontal");
+
+        //transform.position += new Vector3(h, 0, 0) * Time.deltaTime * initialSpeed;
+        rigid.AddForce(Vector2.right * h * Time.deltaTime, ForceMode2D.Impulse);
+
     }
 }
