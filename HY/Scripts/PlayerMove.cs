728b801d1e936b2e7894ee8227558fa155f763df
hoon159487 <41232928+hoon159487@users.noreply.github.com>
2020-10-08 23:23:39 +0900

    앉았을때(0.5x), 엎드렸을때(0.2x) 속도 조절, 점프 1회 제한

--- HY/Scripts/PlayerMove.cs
+++ HY/Scripts/PlayerMove.cs
@@ -7,6 +7,11 @@ public class PlayerMove : MonoBehaviour
     public float maxSpeed;
     public float moveSpeed;
     public float jumpPower;
+    public bool isStand = true;
+    public bool isProne = false;
+    public bool isCrouch = false;
+
+    private bool isGrounded = true;
     SpriteRenderer spriteRenderer;
     Rigidbody2D rigid;
 
@@ -23,21 +28,29 @@ public class PlayerMove : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "ground")
+        {
+            print("1");
+            isGrounded = true;
+        }
+    }
 
+    // Update is called once per frame
     private void Update()
     {
         // Jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigid.velocity.y) < 0.01f)
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isGrounded = false;
         }
 
         // Stop Speed
         if (Input.GetButtonUp("Horizontal"))
         {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
-            print("stop speed");
         }
 
         // Direction Sprite
@@ -49,7 +62,18 @@ public class PlayerMove : MonoBehaviour
         // Moving both side <-, ->
         if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0)
         {
-            transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f));
+            if (isStand)
+            {
+                transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f));
+            }
+            else if (isCrouch)
+            {
+                transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed * 0.5f * Time.deltaTime, 0f));
+            }
+            else if (isProne)
+            {
+                transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed * 0.2f * Time.deltaTime, 0f));
+            }
         }
 
         // seat, prone
@@ -58,18 +82,23 @@ public class PlayerMove : MonoBehaviour
             boxCollider.size = new Vector2(0.836f, 0.3f);
             boxCollider.offset = new Vector2(0f, 0f);
             spriteRenderer.sprite = pronesprite[0];
+            isProne = true; isStand = false; isCrouch = false;
         }
         else if (Input.GetKey(KeyCode.C))
         {
             boxCollider.size = new Vector2(0.3f, 0.6f);
             spriteRenderer.sprite = crouch[0];
+            isProne = false; isStand = false; isCrouch = true;
         }
         else
         {
             boxCollider.size = new Vector2(0.3f, 0.836f);
             boxCollider.offset = new Vector2(0.05f, 0.02f);
             spriteRenderer.sprite = stand[0];
+            isProne = false; isStand = true; isCrouch = false;
         }
+
+
 
     }
     void FixedUpdate()
