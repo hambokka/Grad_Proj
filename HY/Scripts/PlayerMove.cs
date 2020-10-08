3e48f42dbbc8825475cdce2e3336683fd567e818
hoon159487 <41232928+hoon159487@users.noreply.github.com>
2020-10-08 21:18:58 +0900

    좌우움직임, 점프, 앉기, 엎드리기

--- HY/Scripts/PlayerMove.cs
+++ HY/Scripts/PlayerMove.cs
@@ -10,9 +10,15 @@ public class PlayerMove : MonoBehaviour
     SpriteRenderer spriteRenderer;
     Rigidbody2D rigid;
 
+    public Sprite[] stand;
+    public Sprite[] pronesprite;
+    public Sprite[] crouch;
+    private BoxCollider2D boxCollider;
+
     // Start is called before the first frame update
     void Awake()
     {
+        boxCollider = GetComponent<BoxCollider2D>();
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -44,6 +50,25 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0)
         {
             transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f));
+        }
+
+        // seat, prone
+        if (Input.GetKey(KeyCode.Z))
+        {
+            boxCollider.size = new Vector2(0.836f, 0.3f);
+            boxCollider.offset = new Vector2(0f, 0f);
+            spriteRenderer.sprite = pronesprite[0];
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            boxCollider.size = new Vector2(0.3f, 0.6f);
+            spriteRenderer.sprite = crouch[0];
+        }
+        else
+        {
+            boxCollider.size = new Vector2(0.3f, 0.836f);
+            boxCollider.offset = new Vector2(0.05f, 0.02f);
+            spriteRenderer.sprite = stand[0];
         }
 
     }
