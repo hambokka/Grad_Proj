b068d7171cc74120c920f0b15bd83fca6e322fb6
Gwonho.J <sswcg@daum.net>
2020-10-16 22:56:53 +0900

    화면진동구현
    10/16 VibrateForTime() 메소드 이용하여 원하는 시간만큼 진동 매개변수는 float형이다.

---
+++ GH/CameraShake.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+	public float ShakeAmout;
+	//public Canvas canvas;
+	float ShakeTime;
+	Vector3 initialPosition;
+	
+	public void VibrateForTime(float time)
+	{
+		ShakeTime = time;
+		//canvas.renderMode = RenderMode.ScreenSpaceCamera;
+		//canvas.renderMode = RenderMode.WorldSpace;
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = new Vector3(0f, 0f, -10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ShakeTime > 0){
+			transform.position = Random.insideUnitSphere * ShakeAmout + initialPosition;
+			ShakeTime -= Time.deltaTime;
+		}
+		else{
+			ShakeTime = 0.0f;
+			transform.position = initialPosition;
+			//canvas.renderMode = RenderMode.ScreenSpaceCamera;
+		}
+    }
+}
