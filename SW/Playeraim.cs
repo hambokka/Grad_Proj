b4014498edac12b11497feb712aeed84eeee9b21
sangwoo-ryu <59352715+sangwoo-ryu@users.noreply.github.com>
2020-10-26 21:11:47 +0900

    이동 및 조준 방법 수정
    현제 Player_state.cs는 공파일 나중에 에니메이션 처리에 사용할 예정

---
+++ SW/Playeraim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playeraim : MonoBehaviour
+{
+    Transform aimtransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aimtransform = transform.Find("Weapons");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Aim();
+    }
+
+    void Aim()
+    {
+        Vector3 mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 aimdir = (mouseposition - transform.position).normalized;
+
+        float angle = Mathf.Atan2(aimdir.y, aimdir.x) * Mathf.Rad2Deg;
+
+        aimtransform.eulerAngles = new Vector3(0, 0, angle);
+
+        Vector3 localscale = Vector3.one;
+
+        if (angle > 90 || angle < -90)
+            localscale.y = -1f;
+        else
+            localscale.y = 1f;
+        aimtransform.localScale = localscale;
+    }
+}
---
+++ SW/Playermovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Player_state playerstate;
+
+    void Start()
+    {
+        playerstate = gameObject.GetComponent<Player_state>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float moveSpeed = 10f;
+        if(Input.GetButton("Horizontal"))
+        {
+            Vector3 movedir = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+            transform.position += movedir * moveSpeed * Time.deltaTime;
+        }
+    }
+}
