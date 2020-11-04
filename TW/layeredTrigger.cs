50931bf94c7f27e5e97beb84182218fb5129cd3c
PrivateCain <con2220@naver.com>
2020-11-04 22:16:32 +0900

    키 입력을 받고 충돌 ON/OFF 변환하는 코드
    1. 빈 게임오브젝트 하나를 하이에라키에 추가
    2. 해당 게임오브젝트에 해당 스크립트 추가
    3. 플레이어 오브젝트와 지형 오브젝트를 유니티에서 드래그해서 추가

---
+++ TW/layeredTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class layeredTrigger : MonoBehaviour
+{
+
+    
+    bool isOutside = true;
+    public GameObject player;
+    public GameObject terrain;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update() {
+        
+        //if (Input.GetButtonDown("Fire2"))
+        if (Input.GetKey(KeyCode.K))
+        {
+            isOutside = !isOutside; //toggle
+            Debug.Log(isOutside);
+        }
+        
+        if(isOutside == true)
+        {
+            //collision off
+            //GetComponent<TilemapCollider2D>().enabled = false;
+            
+                Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), terrain.GetComponent<Collider2D>(),true);
+        }
+        else
+        {
+            //collision on
+            //GetComponent<TilemapCollider2D>().enabled = true;
+            
+                Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), terrain.GetComponent<Collider2D>(), false);
+        }
+    }
+}
