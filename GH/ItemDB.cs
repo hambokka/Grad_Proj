212224b45bf1e9291363ddf43c46a84e19b3448f
Gwonho.J <sswcg@daum.net>
2020-11-05 21:50:56 +0900

    임시인벤토리 스크립트
    11월초작성

---
+++ GH/ItemDB.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+	public static ItemDB instance;
+	
+    void Awake()
+    {
+        instance = this;
+    }
+	
+	public List<Item>itemDB = new List<Item>();
+	[Space(20)]
+	public GameObject fieldItemPrefab;
+	public Vector3[] pos;
+	
+	void Start()
+	{
+		for(int i=0; i<2; i++){
+			GameObject go = Instantiate(fieldItemPrefab, pos[i], Quaternion.identity);
+			go.GetComponent<FieldItems>().SetItem(itemDB[Random.Range(0,3)]);
+		}
+	}
+}
