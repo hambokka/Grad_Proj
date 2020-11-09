4d80aa76222049111b5a7e66352bcfe63926c85a
PrivateCain <con2220@naver.com>
2020-11-09 21:36:38 +0900

    Added DecoDisappear
    키 입력을 받아서 (코드엔 예시로 K) 지정된 타일맵의 렌더링을 멈추는 코드

---
+++ TW/DecoDisappear.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class DecoDisappear : MonoBehaviour
+{
+
+
+    bool isOutside = true;
+    public GameObject outsideDeco;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            isOutside = !isOutside; //toggle
+            Debug.Log(isOutside);
+        }
+
+        if (isOutside == true)
+        {
+            //collision off
+
+            outsideDeco.GetComponent<TilemapRenderer>().enabled = true;
+        }
+        else
+        {
+            //collision on
+            
+            outsideDeco.GetComponent<TilemapRenderer>().enabled = false;
+        }
+    }
+}
